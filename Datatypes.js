23687f0afa54a763f2a33abebea08f50eff89216
Muhammadmarch09 <muhammadmarch@gmail.com>
2024-03-10 12:30:24 +0600

    adding js

--- Datatypes.js
+++ Datatypes.js
@@ -9,4 +9,22 @@ let  name = "sakib";
 // Null => standable value 
 // undefined => undefined 
 // Symbol => unique 
-console.log(typeof name); //  typeof used to see the datatype
+//console.log(typeof name); //  typeof used to see the datatype
+// NaN >> Not a Numder 
+let score = "Hitesh";
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+//"33" => 33;
+//"33abc" => NaN
+// true =>1;false =>0;
+
+let isLoggedIn = "hitesh";
+
+let boolearIsLoggedIn = Boolean(isLoggedIn);
+//console.log(boolearIsLoggedIn);
+
+// "" => false
+// "hitesh" => true
