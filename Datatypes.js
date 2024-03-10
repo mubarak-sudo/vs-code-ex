e4681f18544737f6605e95a8c1e50fcd92dff6aa
Muhammadmarch09 <muhammadmarch@gmail.com>
2024-03-10 12:01:04 +0600

    another file

---
+++ Datatypes.js
@@ -0,0 +1,12 @@
+let  name = "sakib";
+
+// alert() in node js we can't use alert
+// Learning datatypes
+// string => anything inside ""
+// Boolean => has two value true /false 
+// BigInt => used to big numder 
+// Number => any integer 
+// Null => standable value 
+// undefined => undefined 
+// Symbol => unique 
+console.log(typeof name); //  typeof used to see the datatype
--- variables.js
+++ variables.js
@@ -2,5 +2,8 @@ const accountId = 1010011;
 let accountName = "Mubarak";
 let accountState =  "Dhaka";
 let accountCountry = "Bangladesh";
-
-console.table([accountId,accountName,accountState,accountCountry]);
+const accountPassword =12345678;
+console.table([accountId,accountName,accountState,accountCountry,accountPassword]);
+// Note
+// We cannot change variable diclared with const keyword
+// use let and const instead of var 
